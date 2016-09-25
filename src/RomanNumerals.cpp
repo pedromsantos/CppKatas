b30cd7f4dc0c2c1db2a8f5af0c30fa38ff729527
Pedro Santos <github@mariodelrio.com>
2016-09-25 22:32:03 +0100

    RomanNumeral can convert up to 10

--- src/RomanNumerals.cpp
+++ src/RomanNumerals.cpp
@@ -2,10 +2,25 @@
 
 const std::string RomanNumeral(const unsigned int arabicNumber)
 {
+    if(arabicNumber == 10)
+    {
+        return "X";
+    }
+
+    if(arabicNumber == 9)
+    {
+        return "IX";
+    }
+
+    if(arabicNumber >= 5)
+    {
+        return "V" + RomanNumeral(arabicNumber - 5);
+    }
+
     if(arabicNumber == 4)
     {
         return "IV";
     }
-    
+
     return std::string(arabicNumber, 'I');
 }
--- test/RomanNumeralsTests.cpp
+++ test/RomanNumeralsTests.cpp
@@ -5,4 +5,10 @@ TEST_CASE( "Calculate roman numeral for arabic number", "[Fibonacci]") {
     REQUIRE( RomanNumeral(1) == "I" );
     REQUIRE( RomanNumeral(2) == "II" );
     REQUIRE( RomanNumeral(4) == "IV" );
+    REQUIRE( RomanNumeral(5) == "V" );
+    REQUIRE( RomanNumeral(6) == "VI" );
+    REQUIRE( RomanNumeral(7) == "VII" );
+    REQUIRE( RomanNumeral(8) == "VIII" );
+    REQUIRE( RomanNumeral(9) == "IX" );
+    REQUIRE( RomanNumeral(10) == "X" );
 }
