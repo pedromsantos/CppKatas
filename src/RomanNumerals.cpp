6007714c7d332fb438fd5b8a00656d39695488cd
Pedro Santos <github@mariodelrio.com>
2016-09-25 21:05:06 +0100

    RomanNumeral can convert 4 to IV

--- src/RomanNumerals.cpp
+++ src/RomanNumerals.cpp
@@ -2,5 +2,10 @@
 
 const std::string RomanNumeral(const unsigned int arabicNumber)
 {
+    if(arabicNumber == 4)
+    {
+        return "IV";
+    }
+    
     return std::string(arabicNumber, 'I');
 }
