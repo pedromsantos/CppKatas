9af4d6edd83d08f85732fc03d55975c011b76d7f
Pedro Santos <github@mariodelrio.com>
2018-02-22 01:12:35 +0000

    Made Fibonacci function a constexpr

--- src/Fibonacci.cpp
+++
@@ -1,8 +0,0 @@
-#include "Fibonacci.hpp"
-
-unsigned int Fibonacci(const unsigned short index)
-{
-    return (index == 0 || index == 1)
-           ? index
-           : Fibonacci(index - 1) + Fibonacci(index - 2);
-}
--- src/Fibonacci.hpp
+++ src/Fibonacci.hpp
@@ -1,6 +1,11 @@
 #ifndef CPPKATAS_SRC_FIBONACCI_H
 #define CPPKATAS_SRC_FIBONACCI_H
 
-unsigned int Fibonacci(unsigned short  index);
+constexpr auto Fibonacci(const unsigned short index)
+{
+    if(index == 0) { return 0;}
+    if(index == 1) { return 1;}
+    return Fibonacci(index - 1) + Fibonacci(index - 2);
+}
 
 #endif
