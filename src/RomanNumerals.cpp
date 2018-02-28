f9d5c9e68f8a1e03617f5c20deae4b19cfcc92ce
Pedro Santos <github@mariodelrio.com>
2018-02-28 15:36:04 +0000

    Refactor Roman numerals kata

--- src/RomanNumerals.cpp
+++ src/RomanNumerals.cpp
@@ -1,30 +1,26 @@
-#include <map>
 #include "RomanNumerals.hpp"
+#include <map>
 
 std::string RomanNumeral(const unsigned short arabicNumber)
 {
-    std::map<unsigned int, std::string> arabicsToRomans =
-            {
-                    {1,    "I"},
-                    {4,    "IV"},
-                    {5,    "V"},
-                    {9,    "IX"},
-                    {10,   "X"},
-                    {40,   "XL"},
-                    {50,   "L"},
-                    {90,   "XC"},
-                    {100,  "C"},
-                    {400,  "CD"},
-                    {500,  "D"},
-                    {900,  "CM"},
-                    {1000, "M"},
-            };
+    using numeral = std::pair<int, std::string>;
 
-    for (auto arabicToRoman = arabicsToRomans.rbegin(); arabicToRoman != arabicsToRomans.rend(); ++arabicToRoman)
+    static const numeral arabicsToRomans[] =
+            {
+                {1000, "M"}, {900, "CM"},
+                {500, "D"}, {400, "CD"},
+                {100, "C"}, {90, "XC"},
+                {50, "L"}, {40, "XL"},
+                {10, "X"}, {9, "IX"},
+                {5, "V"}, {4, "IV"},
+                {1, "I"}
+            };
+    
+    for (auto [arabic, roman] : arabicsToRomans)
     {
-        if (arabicNumber >= arabicToRoman->first)
+        if (arabicNumber >= arabic)
         {
-            return arabicToRoman->second + RomanNumeral(arabicNumber - arabicToRoman->first);
+            return roman + RomanNumeral(arabicNumber - arabic);
         }
     }
 
