d365f09d58e212cb843f816eb3f9a2ce0d44ead1
Pedro Santos <github@mariodelrio.com>
2018-03-01 17:24:08 +0000

    Reformat roman numerals again

--- src/RomanNumerals.cpp
+++ src/RomanNumerals.cpp
@@ -1,22 +1,21 @@
 #include "RomanNumerals.hpp"
-#include <map>
 
 std::string RomanNumeral(const unsigned short arabicNumber)
 {
-    using numeral = std::pair<int, std::string>;
+    using arabicToRoman = std::pair<int, std::string>;
 
-    static const numeral arabicsToRomans[] =
-          {
-                  {1000, "M"}, {900, "CM"},
-                  {500, "D"}, {400, "CD"},
-                  {100, "C"}, {90, "XC"},
-                  {50, "L"}, {40, "XL"},
-                  {10, "X"}, {9, "IX"},
-                  {5, "V"}, {4, "IV"},
-                  {1, "I"}
-          };
+    static const arabicToRoman arabicsToRomans[] =
+            {
+                    {1000, "M"}, {900, "CM"},
+                    {500,  "D"}, {400, "CD"},
+                    {100,  "C"}, {90,  "XC"},
+                    {50,   "L"}, {40,  "XL"},
+                    {10,   "X"}, {9,   "IX"},
+                    {5,    "V"}, {4,   "IV"},
+                    {1,    "I"}
+            };
 
-    for (auto [arabic, roman] : arabicsToRomans)
+    for (auto [arabic, roman]: arabicsToRomans)
     {
         if (arabicNumber >= arabic)
         {
